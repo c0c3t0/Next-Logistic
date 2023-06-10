4595cd22aeafd7b421a7d7623a062cd3c81afcb6
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2023-06-10 17:21:16 +0300

    Initial commit

---
+++ next_logistic/next_logistic_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NextLogisticAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'next_logistic.next_logistic_app'
